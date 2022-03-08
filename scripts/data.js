301587cf2d039b25af8754c410c08bc0cbd42dbb
Najmush Saaquib <saaquibnajmush@gmail.com>
2022-03-09 00:04:54 +0530

    sort feature

--- scripts/data.js
+++ scripts/data.js
@@ -1,64 +1,64 @@
 let dataArr = [
-    {
-        img: "https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i",
-        name: "The Shawshank Redemption",
-        genre:"Drama",
-        rating:9.2
-    },
-    {
-        img: "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UY562_CR8,0,380,562_.jpg",
-        name: "The Godfather",
-        genre:"Drama, Crime",
-        rating:8.4
-    },
+  {
+    img: "https://wallpaperaccess.com/full/1632109.jpg",
+    name: "The Shawshank Redemption",
+    genre: "Drama",
+    rating: 9.4,
+  },
+  {
+    img: "https://w0.peakpx.com/wallpaper/557/513/HD-wallpaper-the-godfather-marlon-brando-vito-corleone.jpg",
+    name: "The Godfather",
+    genre: "Drama, Crime",
+    rating: 8.6,
+  },
 
-    {
-        img: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_QL75_UX380_CR0,0,380,562_.jpg",
-        name: "The Dark Knight",
-        genre:"Drama, Crime, Action",
-        rating:7.2
-    },
+  {
+    img: "https://wallpaperaccess.com/full/4594643.jpg",
+    name: "The Dark Knight",
+    genre: "Drama, Crime, Action",
+    rating: 9.0,
+  },
 
-    {
-        img: "https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UY562_CR7,0,380,562_.jpg",
-        name: "The Godather-II",
-        genre:"Drama, Crime",
-        rating:9.0
-    },
+  {
+    img: "https://wallpaperaccess.com/full/6790561.jpg",
+    name: "Spider-Man: No Way Home",
+    genre: "Drama, Crime",
+    rating: 9.2,
+  },
 
-    {
-        img: "https://m.media-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_QL75_UX380_CR0,11,380,562_.jpg",
-        name: "The Angry Men",
-        genre:"Drama, Crime",
-        rating:6.0
-    },
+  {
+    img: "https://cdn.wallpapersafari.com/37/38/AYBiND.jpg",
+    name: "The Angry Men",
+    genre: "Drama, Crime",
+    rating: 6.7,
+  },
 
-    {
-        img: "https://m.media-amazon.com/images/M/MV5BMTg3MDc4ODgyOF5BMl5BanBnXkFtZTgwNzY1ODIyNjM@._V1_QL75_UX380_CR0,19,380,562_.jpg",
-        name: "Schindler's List",
-        genre:"Drama, Biography, History",
-        rating:7.6
-    },
+  {
+    img: "https://themarckoguy.files.wordpress.com/2015/08/movie_classical_schindler_s_list_hands_movie_123903_1366x768.jpg",
+    name: "Schindler's List",
+    genre: "Drama, Biography, History",
+    rating: 7.9,
+  },
 
-    {
-        img: "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_QL75_UX380_CR0,0,380,562_.jpg",
-        name: "The Lord of The Rings",
-        genre:"Drama, Action, Adventure",
-        rating:9.2
-    },
+  {
+    img: "https://wallpaperaccess.com/full/160477.jpg",
+    name: "The Lord of The Rings",
+    genre: "Drama, Action, Adventure",
+    rating: 7.3,
+  },
 
-    {
-        img: "https://www.imdb.com/title/tt0317248/mediaviewer/rm2898330112/?ref_=tt_ov_i",
-        name: "City of God",
-        genre:"Drama,Crime",
-        rating:8.8
-    },
+  {
+    img: "https://www.nolanfans.com/wordpress/wp-content/uploads/2010/06/inception_new_banner2_large.jpg",
+    name: "Inception",
+    genre: "Drama,Thriller, Sci-Fi, Action",
+    rating: 8.8,
+  },
 
-    {
-        img: "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_QL75_UX380_CR0,0,380,562_.jpg",
-        name: "Interstellar",
-        genre:"Adventure, Drama, Sci-fi",
-        rating:9.8
-    },
+  {
+    img: "https://cdn.wallpapersafari.com/97/65/v49Dqr.jpg",
+    name: "Interstellar",
+    genre: "Adventure, Drama, Sci-fi",
+    rating: 9.0,
+  },
 ];
-localStorage.setItem("data", JSON.stringify(dataArr))
+localStorage.setItem("movies", JSON.stringify(dataArr));
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,98 @@
-console.log(dataArr)
+console.log(dataArr);
+
+let id;
+
+let i = 1;
+
+let images = JSON.parse(localStorage.getItem("movies"));
+
+slideshow.innerHTML = null; // So that only a sinle image append at a time
+
+let img = document.createElement("img");
+img.src = images[i].img;
+
+slideshow.append(img);
+
+id = setInterval(function callMe() {
+  if (i === images.length) i = 0; // Making it a loop or kinda circular que
+
+  let image = images[i].img; // access of URL
+
+  slideshow.innerHTML = null; // So that only a sinle image append at a time
+
+  let img = document.createElement("img");
+  img.src = image;
+
+  slideshow.append(img);
+
+  i++;
+}, 3000);
+
+// Appending all the  movies data below
+
+let movieArr = JSON.parse(localStorage.getItem("movies"));
+
+display(movieArr);
+
+function display(movieArr) {
+  document.querySelector("#movies").innerHTML = null;
+
+  movieArr.map(function (el, ind) {
+    let container = document.createElement("div");
+    container.setAttribute("id", "container");
+
+    let image = document.createElement("img");
+    image.src = el.img;
+
+    let title = document.createElement("h2");
+    title.innerHTML = el.name;
+    title.setAttribute("id", "title");
+
+    let genre = document.createElement("p");
+    genre.innerHTML = "Genre: " + el.genre;
+    genre.setAttribute("id", "genre");
+
+    let rating = document.createElement("p");
+    rating.innerHTML = "Rating: " + el.rating + "⭐";
+    rating.setAttribute("id", "rating");
+
+    container.append(image, title, genre, rating);
+
+    document.querySelector("#movies").append(container);
+  });
+}
+
+// ! Sorting Functinality
+
+function sortme() {
+  let sort = document.querySelector("#sort").value;
+
+  if (sort == "") {
+    display(movieArr);
+    console.log("Empty Sort");
+  } else if (sort == "sort-lh") {
+    for (let i = 0; i < movieArr.length - 1; i++) {
+      for (let j = 0; j < movieArr.length - i - 1; j++) {
+        if (movieArr[j].rating > movieArr[j + 1].rating) {
+          swap(j, j + 1, movieArr);
+        }
+      }
+    }
+    display(movieArr);
+    console.log(movieArr);
+  } else if (sort == "sort-hl") {
+    for (let i = 0; i < movieArr.length - 1; i++) {
+      for (let j = 0; j < movieArr.length - i - 1; j++) {
+        if (movieArr[j].rating < movieArr[j + 1].rating) {
+          swap(j, j + 1, movieArr);
+        }
+      }
+    }
+    display(movieArr);
+    console.log(movieArr);
+  }
+}
+
+function swap(a, b, arr) {
+  [arr[a], arr[b]] = [arr[b], arr[a]];
+}
